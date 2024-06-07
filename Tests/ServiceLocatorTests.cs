4cf799e5bc97de0bbd03429564b4d3c44660ad4f
Zenvin <zenvin@outlook.de>
2024-06-07 19:55:41 +0200

    - Update Tests

--- Tests/ServiceLocatorTests.cs
+++ Tests/ServiceLocatorTests.cs
@@ -4,13 +4,16 @@ using Zenvin.ServiceLocator;
 public partial class ServiceLocatorTests {
 	[Test]
 	public void GlobalServiceRegistrationSucceeds () {
+		ServiceLocator.ResetAll ();
+
 		// Arrange
+		var locator = new ServiceLocator ();
 		var service = new TestService ();
 		var success = true;
 
 		// Act
-		ServiceLocator.Global.Register (service);
-		ServiceLocator.Global.Get (out service, delegate { success = false; });
+		locator.Register (service);
+		locator.Get (out service, delegate { success = false; });
 
 		// Assert
 		Assert.NotNull (service);
@@ -19,6 +22,8 @@ public partial class ServiceLocatorTests {
 
 	[Test]
 	public void ContextualizedServiceRegistrationSucceeds () {
+		ServiceLocator.ResetAll ();
+
 		// Arrange
 		var service = new TestService ();
 		var context = new TestContext (1);
@@ -31,5 +36,19 @@ public partial class ServiceLocatorTests {
 		// Assert
 		Assert.NotNull (service);
 		Assert.IsTrue (success);
+	}
+
+	[Test]
+	public void ContextAccessActivatesProxy () {
+		ServiceLocator.ResetAll ();
+
+		// Arrange
+		var context = new TestContext (1);
+
+		// Act
+		var locator = ServiceLocator.ForContext(context);
+
+		// Assert
+		Assert.IsInstanceOf<ServiceLocator.Proxy> (locator);
 	}
 }
