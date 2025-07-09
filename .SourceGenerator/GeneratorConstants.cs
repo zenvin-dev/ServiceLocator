2eab486f848d72dd70e4e5bd64e41bf15b38608c
Zenvin <zenvin@outlook.de>
2025-07-09 23:38:21 +0200

    - Add SourceGenerator project

---
+++ .SourceGenerator/GeneratorConstants.cs
@@ -0,0 +1,15 @@
+﻿namespace Zenvin.Services.SourceGenerator
+{
+	internal static class GeneratorConstants
+	{
+		public const string InjectServicesMethodName = "InjectServices";
+		public const string GetMethodName = "Get";
+		public const string RequiredArgumentName = "required";
+		public const string AttrArgNameContractType = "ContractType";
+		public const string AttrArgNameRequired = "Required";
+
+		public const string ClassAttributeName = "Zenvin.Services.Attributes.ServiceDependencyAttribute";
+		public const string MemberAttributeName = "Zenvin.Services.Attributes.InjectServiceAttribute";
+		public const string ServiceLocatorType = "Zenvin.Services.ServiceLocator";
+	}
+}
---
+++ .SourceGenerator/ServiceInstallerReceiver.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Generic;
+using System.Linq;
+using static Zenvin.Services.SourceGenerator.GeneratorConstants;
+
+namespace Zenvin.Services.SourceGenerator
+{
+	internal class ServiceInstallerReceiver : ISyntaxReceiver
+	{
+		public readonly List<ClassDeclarationSyntax> DependentClasses = new List<ClassDeclarationSyntax> ();
+
+
+		void ISyntaxReceiver.OnVisitSyntaxNode (SyntaxNode syntaxNode)
+		{
+			// Node is not a class declaration
+			if (!(syntaxNode is ClassDeclarationSyntax classNode))
+				return;
+
+			// Class is a nested type
+			if (!(classNode.Parent is CompilationUnitSyntax) && !(classNode.Parent is NamespaceDeclarationSyntax))
+				return;
+
+			// Class is not partial
+			if (!classNode.HasLeadingTrivia || !classNode.GetLeadingTrivia ().Any (trivia => trivia.IsKind (SyntaxKind.PartialKeyword)))
+				return;
+
+			// Class has no attributes
+			if (classNode.AttributeLists.Count == 0)
+				return;
+
+			// Class does not have RELEVANT attribute
+			if (!HasAttribute (classNode.AttributeLists, ClassAttributeName))
+				return;
+
+			DependentClasses.Add (classNode);
+		}
+
+		private static bool HasAttribute (SyntaxList<AttributeListSyntax> attributeLists, string attributeName)
+		{
+			return attributeLists
+			.SelectMany (al => al.Attributes)
+			.Any (attr =>
+			{
+				var name = attr.Name.ToString ();
+				return name == attributeName ||
+					   name == $"{attributeName}Attribute" ||
+					   name.EndsWith ($".{attributeName}") ||
+					   name.EndsWith ($".{attributeName}Attribute");
+			});
+		}
+	}
+}
