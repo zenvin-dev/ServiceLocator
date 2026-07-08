0d35c05f09c63c55ebe5a8092f49178c877625f8
Zenvin <zenvin@outlook.de>
2026-07-08 21:34:21 +0200

    Add registration utility

--- Runtime/Unity/ServiceLocatorInitializerExtensions.cs
+++ Runtime/Unity/ServiceLocatorInitializerExtensions.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using Zenvin.Services.Core;
 
@@ -9,6 +10,39 @@ namespace Zenvin.Services.Unity
 		{
 			var wrapper = new UnityLoggerWrapper (logger);
 			return init?.WithLogger (wrapper);
+		}
+
+		public static void AddObjectsToScope (this ServiceScopeBuilder builder, IEnumerable<object> services, bool allowDirectServiceInterface, string source = null)
+		{
+			foreach (var obj in services)
+			{
+				if (!builder.AddObjectToScope (obj, allowDirectServiceInterface))
+				{
+					Debug.LogWarning ($"[Bootstrapper] Cannot register object '{obj}' ({obj.GetType ().FullName}) as Service (Source: {(source ?? "<unknown>")}).");
+				}
+			}
+		}
+
+		public static bool AddObjectToScope (this ServiceScopeBuilder builder, object service, bool allowDirectServiceInterface)
+		{
+			switch (service)
+			{
+				case ScriptableObject so:
+					builder.RegisterServiceInstance (so);
+					return true;
+				case GameObject go:
+					builder.RegisterPrefabByProxy (go);
+					return true;
+				case IService srv:
+					if (allowDirectServiceInterface)
+					{
+						builder.RegisterInstance (srv.ContractType, srv);
+						return true;
+					}
+					return false;
+				default:
+					return false;
+			}
 		}
 	}
 }
